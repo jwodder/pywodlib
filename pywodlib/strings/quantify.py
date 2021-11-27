9eff8e3797c0d102efd452ada821c3ebb8c0d765
John T. Wodder II <git@varonathe.org>
2021-11-26 23:29:34 -0500

    quantify()

---
+++ pywodlib/strings/quantify.py
@@ -0,0 +1,8 @@
+def quantify(qty: int, singular: str, plural: Optional[str] = None) -> str:
+    # cf. the humanfriendly package's pluralize() function
+    if qty == 1:
+        return f"{qty} {singular}"
+    elif plural is None:
+        return f"{qty} {singular}s"
+    else:
+        return f"{qty} {plural}"
