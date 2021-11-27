448c6ca96395539a60ed3de2d3047d9f815fe4f8
John T. Wodder II <git@varonathe.org>
2021-11-26 23:21:06 -0500

    Make bytes2iso() aware of ZB and YB

--- pywodlib/bytes2iso.py
+++ pywodlib/bytes2iso.py
@@ -9,12 +9,12 @@ def bytes2iso(numbytes):
     '976.56 kB'
     >>> bytes2iso(2334597576389)
     '2.12 TB'
-    >>> bytes2iso(1152921504606846976000000)
-    '1000000.00 EB'
+    >>> bytes2iso(1208925819614629174706176000000)
+    '1000000.00 YB'
     """
     size = numbytes
     sizestr = f"{size} B"
-    for prefix in "kMGTPE":
+    for prefix in "kMGTPEZY":
         if size < 1024:
             return sizestr
         size /= 1024
