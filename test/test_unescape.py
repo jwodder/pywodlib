91aff9449fba33d3b1218fd029947f65543368d2
John T. Wodder II <git@varonathe.org>
2022-01-23 14:42:51 -0500

    Test `unescape()`

---
+++ test/test_unescape.py
@@ -0,0 +1,33 @@
+import pytest
+from pywodlib.strings.unescape import unescape
+
+
+@pytest.mark.parametrize(
+    "src,dest",
+    [
+        ("foo", "foo"),
+        (r"foo\n", "foo\n"),
+        (r"foo\\n", "foo\\n"),
+        (r"foo\\\n", "foo\\\n"),
+        (r"foo\012", "foo\n"),
+        (r"foo\x0A", "foo\n"),
+        (r"foo\u000A", "foo\n"),
+        (r"foo\\bar", r"foo\bar"),
+        (r"foo\'bar", "foo'bar"),
+        (r"foo\"bar", 'foo"bar'),
+        (r"foo\abar", "foo\abar"),
+        (r"foo\bbar", "foo\bbar"),
+        (r"foo\fbar", "foo\fbar"),
+        (r"foo\rbar", "foo\rbar"),
+        (r"foo\tbar", "foo\tbar"),
+        (r"foo\vbar", "foo\vbar"),
+        (r"\U0001F410", "\U0001F410"),
+        ("åéîøü", "åéîøü"),
+        (r"\u2603", "\u2603"),
+        ("\u2603", "\u2603"),
+        ("\U0001F410", "\U0001F410"),
+        (r"\N{SNOWMAN}", "\u2603"),
+    ],
+)
+def test_unescape(src: str, dest: str) -> None:
+    assert unescape(src) == dest
