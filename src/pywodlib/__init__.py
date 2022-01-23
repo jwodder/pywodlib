154413e5ad61929f2acc1f833645d43431aa860f
John T. Wodder II <git@varonathe.org>
2022-01-23 13:14:02 -0500

    Packaging

---
+++ src/pywodlib/__init__.py
@@ -0,0 +1,11 @@
+"""
+Assorted utility functions
+
+Visit <https://github.com/jwodder/pywodlib> for more information.
+"""
+
+__version__ = "0.1.0.dev1"
+__author__ = "John Thorvald Wodder II"
+__author_email__ = "pywodlib@varonathe.org"
+__license__ = "MIT"
+__url__ = "https://github.com/jwodder/pywodlib"
