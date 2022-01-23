bbe3ec393648e31fd88b47607f2e8553a741cdba
John T. Wodder II <git@varonathe.org>
2022-01-23 15:56:41 -0500

    `serve_path()`

---
+++ src/pywodlib/serve_path.py
@@ -0,0 +1,29 @@
+from contextlib import contextmanager
+from functools import partial
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from pathlib import Path
+from threading import Thread
+from typing import Iterator, Union
+
+
+@contextmanager
+def serve_path(
+    dirpath: Union[str, Path], bind_address: str = "127.0.0.1"
+) -> Iterator[str]:
+    with HTTPServer(
+        (bind_address, 0), partial(SimpleHTTPRequestHandler, directory=dirpath)
+    ) as httpd:
+        t = Thread(target=httpd.serve_forever)
+        t.start()
+        try:
+            server_name, *_ = httpd.server_address
+            # `httpd.server_name` is run through `socket.getfqdn()`, which (on
+            # Python 3.9 on macOS, at least) converts IP addresses to .arpa
+            # addresses, which don't work with requests, so we can't use that.
+            if ":" in server_name:
+                yield f"http://[{server_name}]:{httpd.server_port}"
+            else:
+                yield f"http://{server_name}:{httpd.server_port}"
+        finally:
+            httpd.shutdown()
+            t.join()
---
+++ test/test_serve_path.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import pytest
+import requests
+from pywodlib.serve_path import serve_path
+
+DATA_DIR = Path(__file__).with_name("data")
+
+
+@pytest.mark.parametrize("bind_address", ["127.0.0.1", "localhost", ""])
+def test_serve_path(bind_address: str) -> None:
+    with serve_path(
+        DATA_DIR, bind_address=bind_address
+    ) as base_url, requests.Session() as s:
+        for p in DATA_DIR.iterdir():
+            r = s.get(f"{base_url}/{p.name}")
+            r.raise_for_status()
+            assert r.content == p.read_bytes()
