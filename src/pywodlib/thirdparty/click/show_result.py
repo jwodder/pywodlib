3f421da910ae2498611838e682354e443d0d202a
John T. Wodder II <git@varonathe.org>
2022-06-17 20:33:04 -0400

    Add `show_result()`

---
+++ src/pywodlib/thirdparty/click/show_result.py
@@ -0,0 +1,9 @@
+from traceback import format_exception
+from click.testing import Result
+
+def show_result(r: Result) -> str:
+    if r.exception is not None:
+        assert isinstance(r.exc_info, tuple)
+        return "".join(format_exception(*r.exc_info))
+    else:
+        return r.output
