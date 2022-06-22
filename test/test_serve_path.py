a44541297ea80d5db539759e761e41d50f245e1c
John T. Wodder II <git@varonathe.org>
2022-06-22 11:39:25 -0400

    Fix a security issue in the tests

--- test/test_serve_path.py
+++ test/test_serve_path.py
@@ -6,7 +6,19 @@ from pywodlib.serve_path import serve_path
 DATA_DIR = Path(__file__).with_name("data")
 
 
-@pytest.mark.parametrize("bind_address", ["127.0.0.1", "localhost", ""])
+@pytest.mark.parametrize(
+    "bind_address",
+    [
+        "127.0.0.1",
+        "localhost",
+        pytest.param(
+            "",
+            marks=pytest.mark.skip(
+                reason="Binding to all addresses runs afoul of macOS security"
+            ),
+        ),
+    ],
+)
 def test_serve_path(bind_address: str) -> None:
     with serve_path(
         DATA_DIR, bind_address=bind_address
