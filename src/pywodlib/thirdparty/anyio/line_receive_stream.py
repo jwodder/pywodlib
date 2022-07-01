1505dc17152eac5c2b3ff366cf850203c372275c
John T. Wodder II <git@varonathe.org>
2022-07-01 17:55:49 -0400

    LineReceiveStream: docs and `extra_attributes`

--- src/pywodlib/thirdparty/anyio/line_receive_stream.py
+++ src/pywodlib/thirdparty/anyio/line_receive_stream.py
@@ -1,13 +1,29 @@
-from typing import Optional
+from __future__ import annotations
+from collections.abc import Callable, Mapping
+from typing import Any, Optional
 from anyio import EndOfStream
 from anyio.abc import ObjectReceiveStream
 from linesep import SplitterEmptyError, get_newline_splitter
 
 
 class LineReceiveStream(ObjectReceiveStream[str]):
+    """
+    Stream wrapper that splits strings from ``transport_stream`` on newlines
+    and returns each line individually.  Requires the linesep_ package.
+
+    .. _linesep: https://github.com/jwodder/linesep
+    """
+
     def __init__(
         self, transport_stream: ObjectReceiveStream[str], newline: Optional[str] = None
     ) -> None:
+        """
+        :param transport_stream: any `str`-based receive stream
+        :param newline:
+            controls how universal newlines mode works; has the same set of
+            allowed values and semantics as the ``newline`` argument to
+            `open()`
+        """
         self._stream = transport_stream
         self._splitter = get_newline_splitter(newline, retain=True)
 
@@ -24,3 +40,7 @@ class LineReceiveStream(ObjectReceiveStream[str]):
 
     async def aclose(self) -> None:
         await self._stream.aclose()
+
+    @property
+    def extra_attributes(self) -> Mapping[Any, Callable[[], Any]]:
+        return self._stream.extra_attributes
