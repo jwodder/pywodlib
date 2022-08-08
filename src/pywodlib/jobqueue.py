829f386d7b42815d4a1a72650805d6ee8aabe296
John T. Wodder II <git@varonathe.org>
2022-08-08 15:25:23 -0400

    jobqueue.py manages jobs, not tasks

--- src/pywodlib/jobqueue.py
+++ src/pywodlib/jobqueue.py
@@ -1,20 +1,20 @@
 """
 Synchronized queues (both FIFO and LIFO) for use by a collection of concurrent
 workers that are both producers and consumers.  Specifically, after the queue
-is initialized with some starting tasks, each worker iterates through the queue
-task by task as available; for each task, the worker operating on it adds some
-number of new tasks to the queue and then marks the current task finished.
-Once the queue is empty and all tasks have been marked finished, the iterators
-stop yielding values.
+is initialized with some starting jobs, each worker iterates through the queue
+job by job as available; for each job, the worker operating on it adds some
+number of new jobs to the queue and then marks the current job finished.  Once
+the queue is empty and all jobs have been marked finished, the iterators stop
+yielding values.
 
 Sample usage by a worker:
 
 .. code:: python
 
-    for taskctx in job_queue:
-        with taskctx as task:
-            # Operate on task
-            # Call job_queue.put(new_task) some number of times
+    for jobctx in job_queue:
+        with jobctx as job:
+            # Operate on job
+            # Call job_queue.put(new_job) some number of times
 
 Based on <https://gist.github.com/jart/0a71cde3ca7261f77080a3625a21672b>
 """
@@ -35,10 +35,10 @@ class AbstractJobQueue(ABC, Generic[T]):
         self._lock = Lock()
         self._cond = Condition(self._lock)
         self._queue: deque[T] = deque()
-        self._tasks = 0
+        self._jobs = 0
         if iterable is not None:
             self._queue.extend(iterable)
-            self._tasks += len(self._queue)
+            self._jobs += len(self._queue)
 
     @abstractmethod
     def _get(self) -> T:
@@ -48,7 +48,7 @@ class AbstractJobQueue(ABC, Generic[T]):
         while True:
             with self._lock:
                 while True:
-                    if not self._tasks:
+                    if not self._jobs:
                         return
                     if not self._queue:
                         self._cond.wait()
@@ -63,14 +63,14 @@ class AbstractJobQueue(ABC, Generic[T]):
             yield value
         finally:
             with self._lock:
-                self._tasks -= 1
-                if self._tasks <= 0:
+                self._jobs -= 1
+                if self._jobs <= 0:
                     self._cond.notify_all()
 
     def put(self, value: T) -> None:
         with self._lock:
             self._queue.append(value)
-            self._tasks += 1
+            self._jobs += 1
             self._cond.notify()
 
 
